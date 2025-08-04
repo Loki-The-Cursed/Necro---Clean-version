162094a9b285419b5cfb27b6356bf887babac69c
ŁØҚɪ <loki.the.cursed@gmail.com>
2025-08-04 13:36:59 +0800

    Add files via upload

---
+++ my_utility/spell_data.lua
@@ -0,0 +1,76 @@
+local spell_data = {
+    -- Active spells with their spell IDs
+    blood_mist = {
+        spell_id = 493422
+    },
+    bone_spear = {
+        spell_id = 432258
+    },
+    bone_splinters = {
+        spell_id = 427557
+    },
+    corpse_explosion = {
+        spell_id = 432897
+    },
+    corpse_tendrils = {
+        spell_id = 463349
+    },
+    decrepify = {
+        spell_id = 434035
+    },
+    hemorrhage = {
+        spell_id = 484661
+    },
+    reap = {
+        spell_id = 432896
+    },
+    blood_lance = {
+        spell_id = 501629
+    },
+    blood_surge = {
+        spell_id = 592163
+    },
+    blight = {
+        spell_id = 481293
+    },
+    sever = {
+        spell_id = 481785
+    },
+    bone_prison = {
+        spell_id = 493453
+    },
+    iron_maiden = {
+        spell_id = 493195
+    },
+    bone_spirit = {
+        spell_id = 495653
+    },
+    blood_wave = {
+        spell_id = 592435
+    },
+    army_of_the_dead = {
+        spell_id = 493644
+    },
+    bone_storm = {
+        spell_id = 493622
+    },
+    raise_skeleton = {
+        spell_id = 1059157
+    },
+    golem_control = {
+        spell_id = 433402
+    },
+    soulrift = {
+        spell_id = 1644584
+    },
+    decompose = {
+        spell_id = 463175
+    },
+    
+    -- Evade (universal)
+    evade = {
+        spell_id = 337031
+    }
+}
+
+return spell_data
